10c1e5cf96d92ae3b9b0d72678b1fb792c339e5e
seerdms <zhengjunweimail@163.com>
2015-08-22 10:00:26 +0800

    1

--- smart-protocol-p2p/src/test/java/net/vinote/smart/socket/protocol/p2p/client/P2PClient.java
+++ smart-protocol-p2p/src/test/java/net/vinote/smart/socket/protocol/p2p/client/P2PClient.java
@@ -1,12 +1,19 @@
 package net.vinote.smart.socket.protocol.p2p.client;
 
 import java.util.Properties;
+import java.util.logging.Level;
 
 import net.vinote.smart.socket.lang.QuicklyConfig;
+import net.vinote.smart.socket.lang.StringUtils;
+import net.vinote.smart.socket.logger.RunLogger;
 import net.vinote.smart.socket.protocol.P2PProtocolFactory;
 import net.vinote.smart.socket.protocol.p2p.BaseMessageFactory;
 import net.vinote.smart.socket.protocol.p2p.HeartMessageReq;
 import net.vinote.smart.socket.protocol.p2p.HeartMessageResp;
+import net.vinote.smart.socket.protocol.p2p.RemoteInterfaceMessageReq;
+import net.vinote.smart.socket.protocol.p2p.RemoteInterfaceMessageResp;
+import net.vinote.smart.socket.protocol.p2p.processor.RemoteInterface;
+import net.vinote.smart.socket.protocol.p2p.processor.RemoteModel;
 import net.vinote.smart.socket.service.filter.SmartFilter;
 import net.vinote.smart.socket.service.filter.impl.FlowControlFilter;
 import net.vinote.smart.socket.transport.nio.NioQuickClient;
@@ -15,6 +22,7 @@ public class P2PClient {
 	public static void main(String[] args) throws Exception {
 		Properties properties = new Properties();
 		properties.put(HeartMessageResp.class.getName(), "");
+		properties.put(RemoteInterfaceMessageResp.class.getName(), "");
 		BaseMessageFactory.getInstance().loadFromProperties(properties);
 		QuicklyConfig config = new QuicklyConfig(false);
 		P2PProtocolFactory factory = new P2PProtocolFactory();
@@ -27,13 +35,29 @@ public class P2PClient {
 		NioQuickClient client = new NioQuickClient(config);
 		client.start();
 
-		long num = Long.MAX_VALUE;
+		long num = 1;
 		long start = System.currentTimeMillis();
 		while (num-- > 0) {
 			HeartMessageReq req = new HeartMessageReq();
 			processor.getSession().sendWithoutResponse(req);
 		}
 		System.out.println("结束" + (System.currentTimeMillis() - start));
+
+		// 模拟SOFA的TR服务
+		RemoteInterfaceMessageReq req = new RemoteInterfaceMessageReq();
+		req.setInterfaceClass(RemoteInterface.class.getName());// 远程接口名称
+		req.setMethod("say1");// 调用接口方法
+		req.setParamClassList(RemoteModel.class.getName());// 接口方法参数类型
+		RemoteModel model = new RemoteModel();
+		model.setName("zjw1");
+		req.setParams(model);// 参数对象
+		RemoteInterfaceMessageResp data = (RemoteInterfaceMessageResp) processor.getSession().sendWithResponse(req);
+		if (StringUtils.isNotBlank(data.getException())) {
+			System.out.println(data.getException());
+		} else {
+			System.out.println(data.getReturnObject());
+		}
+		RunLogger.getLogger().log(Level.SEVERE, StringUtils.toHexString(data.getData()));
 		client.shutdown();
 	}
 }
