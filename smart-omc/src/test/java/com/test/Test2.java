97c5a4455e9d1def5c06eb8fbbb35dc7fe84324b
wb-zhengjunwei <wb-zhengjunwei@alibaba-inc.com>
2015-08-24 14:06:10 +0800

    删除工程

--- smart-omc/src/test/java/com/test/Test2.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-import sun.security.util.Debug;
-
-public class Test2 {
-	public static void main(String[] args) {
-		System.out.println(Debug.toString("adfaf".getBytes()));
-	}
-}
