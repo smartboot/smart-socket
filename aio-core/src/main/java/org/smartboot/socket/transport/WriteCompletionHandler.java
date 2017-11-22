de62050ba1334bc511b0a51f03291a3e190cc706
Seer <zhengjunweimail@163.com>
2017-11-22 21:35:02 +0800

    代码优化

--- aio-core/src/main/java/org/smartboot/socket/transport/WriteCompletionHandler.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/WriteCompletionHandler.java
@@ -1,12 +1,17 @@
 package org.smartboot.socket.transport;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.smartboot.socket.Filter;
 import org.smartboot.socket.util.StateMachineEnum;
+
+import java.nio.channels.CompletionHandler;
 
 /**
  * 读写事件回调处理类
  */
-class WriteCompletionHandler<T> extends AbstractCompletionHandler<T> {
+class WriteCompletionHandler<T> implements CompletionHandler<Integer, AioSession<T>> {
+    private static final Logger LOGGER = LogManager.getLogger(WriteCompletionHandler.class);
 
     @Override
     public void completed(final Integer result, final AioSession<T> aioSession) {
@@ -19,7 +24,12 @@ class WriteCompletionHandler<T> extends AbstractCompletionHandler<T> {
 
     @Override
     public void failed(Throwable exc, AioSession<T> aioSession) {
-        super.failed(exc, aioSession);
+        LOGGER.catching(exc);
+        try {
+            aioSession.close();
+        } catch (Exception e) {
+            LOGGER.catching(e);
+        }
         aioSession.getServerConfig().getProcessor().stateEvent(aioSession, StateMachineEnum.OUTPUT_EXCEPTION, exc);
     }
 }
