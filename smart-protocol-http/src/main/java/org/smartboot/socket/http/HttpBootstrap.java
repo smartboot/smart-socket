32fbc592c0b5e187ba4c26cf5699293af592a061
Seer <zhengjunweimail@163.com>
2018-02-06 22:21:38 +0800

    调试http协议

--- smart-protocol-http/src/main/java/org/smartboot/socket/http/HttpBootstrap.java
+++ smart-protocol-http/src/main/java/org/smartboot/socket/http/HttpBootstrap.java
@@ -9,8 +9,10 @@
 package org.smartboot.socket.http;
 
 import org.smartboot.socket.Filter;
+import org.smartboot.socket.extension.ssl.ClientAuth;
 import org.smartboot.socket.extension.timer.QuickMonitorTimer;
 import org.smartboot.socket.transport.AioQuickServer;
+import org.smartboot.socket.transport.AioSSLQuickServer;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -18,9 +20,29 @@ import java.net.UnknownHostException;
 public class HttpBootstrap {
 
     public static void main(String[] args) throws UnknownHostException {
+        http();
+    }
+
+    static void http() {
         // 定义服务器接受的消息类型以及各类消息对应的处理器
         AioQuickServer<HttpRequest> server = new AioQuickServer<HttpRequest>(8888, new HttpProtocol(), new HttpMessageProcessor());
         server.setFilters(new Filter[]{new QuickMonitorTimer<HttpRequest>()});
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void https() {
+        // 定义服务器接受的消息类型以及各类消息对应的处理器
+        AioSSLQuickServer<HttpRequest> server = new AioSSLQuickServer<HttpRequest>(8889, new HttpProtocol(), new HttpMessageProcessor());
+        server
+                .setClientAuth(ClientAuth.OPTIONAL)
+                .setKeyStore("server.jks", "storepass")
+                .setTrust("trustedCerts.jks", "storepass")
+                .setKeyPassword("keypass")
+        ;
         try {
             server.start();
         } catch (IOException e) {
