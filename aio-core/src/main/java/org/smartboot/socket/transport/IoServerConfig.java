222eb39a7157103e78c5855e1fd62ae245cc103a
Seer <zhengjunweimail@163.com>
2018-02-26 19:28:09 +0800

    升级至v1.3.2版

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -27,7 +27,7 @@ final class IoServerConfig<T> {
             "\\__, \\| ( ) ( ) |( (_| || |   | |_    \\__, \\( (_) )( (___ | |\\`\\ (  ___/| |_ \n" +
             "(____/(_) (_) (_)`\\__,_)(_)   `\\__)   (____/`\\___/'`\\____)(_) (_)`\\____)`\\__)";
 
-    public static final String VERSION = "v1.3.2-SNAPSHOT";
+    public static final String VERSION = "v1.3.2";
     /**
      * 消息队列缓存大小
      */
