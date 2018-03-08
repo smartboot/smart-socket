9c20a8cc4ee36432be8ee02a3cc6c92687340305
Seer <zhengjunweimail@163.com>
2018-03-08 19:57:13 +0800

    日志输出

--- smart-protocol-p2p/bin/client.sh
+++ smart-protocol-p2p/bin/client.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Djava.ext.dirs=../lib/ org.smartboot.socket.protocol.p2p.client.P2PMultiClient
+curPwd=`pwd`
+java -Dlog4j.configurationFile=file:${curPwd}/../conf/log4j2_client.xml -Djava.ext.dirs=../lib/ org.smartboot.socket.protocol.p2p.client.P2PMultiClient
--- smart-protocol-p2p/bin/server.sh
+++ smart-protocol-p2p/bin/server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -cp ../conf -Djava.ext.dirs=../lib/ org.smartboot.socket.protocol.p2p.server.P2PServer
+curPwd=`pwd`
+java -Dlog4j.configurationFile=file:${curPwd}/../conf/log4j2_server.xml -Djava.ext.dirs=../lib/ org.smartboot.socket.protocol.p2p.server.P2PServer
