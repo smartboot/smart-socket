106b806a5d98739b87b31a7e89eae76c63f73e75
三刀 <junwei.zheng@hipac.cn>
2018-03-27 19:52:29 +0800

    打包

--- smart-protocol-p2p/bin/client.sh
+++ smart-protocol-p2p/bin/client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curPwd=`pwd`
-java -Dlog4j.configurationFile=file:${curPwd}/../conf/log4j2_client.xml -Djava.ext.dirs=../lib/ org.smartboot.socket.protocol.p2p.client.P2PMultiClient
+java -Dlog4j.configurationFile=file:${HTTP_HOME}/conf/log4j2_client.xml -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${HTTP_HOME}/lib/ org.smartboot.socket.protocol.p2p.client.P2PMultiClient
--- smart-protocol-p2p/bin/server.sh
+++ smart-protocol-p2p/bin/server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curPwd=`pwd`
-java -Dlog4j.configurationFile=file:${curPwd}/../conf/log4j2_server.xml -Djava.ext.dirs=../lib/ org.smartboot.socket.protocol.p2p.server.P2PServer
+HTTP_HOME=$(dirname $(pwd))
+java -Dlog4j.configurationFile=file:${HTTP_HOME}/conf/log4j2_server.xml -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${HTTP_HOME}/lib/ org.smartboot.socket.protocol.p2p.server.P2PServer
