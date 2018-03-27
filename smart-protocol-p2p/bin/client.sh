88b6e5f8517f19b7d174a89a0c5ef2cd6f140e4b
三刀 <junwei.zheng@hipac.cn>
2018-03-27 20:01:47 +0800

    打包

--- smart-protocol-p2p/bin/client.sh
+++ smart-protocol-p2p/bin/client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curPwd=`pwd`
+HTTP_HOME=$(dirname $(pwd))
 java -Dlog4j.configurationFile=file:${HTTP_HOME}/conf/log4j2_client.xml -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${HTTP_HOME}/lib/ org.smartboot.socket.protocol.p2p.client.P2PMultiClient
