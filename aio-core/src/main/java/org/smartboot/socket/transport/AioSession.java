37b256f9f9c3c7a58cfbc6c83edfa0474eb15254
三刀 <junwei.zheng@hipac.cn>
2018-04-04 14:43:23 +0800

    测试伪共享

--- aio-core/src/main/java/org/smartboot/socket/transport/AioSession.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/AioSession.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smartboot.socket.Filter;
 import org.smartboot.socket.StateMachineEnum;
-import sun.misc.Contended;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -54,9 +53,7 @@ public class AioSession<T> {
      * 底层通信channel对象
      */
     protected AsynchronousSocketChannel channel;
-    @Contended
     protected ByteBuffer readBuffer;
-    @Contended
     protected ByteBuffer writeBuffer;
     /**
      * 会话当前状态
