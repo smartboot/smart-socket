9c81ab206aebd29a3837e7b61f24aa79757f28dd
三刀 <junwei.zheng@hipac.cn>
2018-04-27 11:11:52 +0800

    移除module

--- smart-protocol-mqtt/src/main/java/org/smartboot/socket/mqtt/MqttContext.java
+++
@@ -1,9 +0,0 @@
-package org.smartboot.socket.mqtt;
-
-/**
- * @author 三刀
- * @version V1.0 , 2018/4/26
- */
-public interface MqttContext {
-    public MqttSession addSession(MqttSession session);
-}
