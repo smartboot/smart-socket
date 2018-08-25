a4cd5aa27ede86ef7782f8b20ae5eb08f362ee35
三刀 <junwei.zheng@hipac.cn>
2018-08-25 13:04:46 +0800

    补充javadoc

--- aio-core/src/main/java/org/smartboot/socket/NetMonitor.java
+++ aio-core/src/main/java/org/smartboot/socket/NetMonitor.java
@@ -11,8 +11,17 @@ package org.smartboot.socket;
 import org.smartboot.socket.transport.AioSession;
 
 /**
- * 网络监控器
+ * 网络监控器，提供通讯层面监控功能的接口。
+ * <p>
+ * smart-socket并未单独提供配置监控服务的接口，用户在使用时仅需在MessageProcessor实现类中同时实现当前NetMonitor接口即可。
+ * 在注册消息处理器时，若服务监测到该处理器同时实现了NetMonitor接口，则该监视器便会生效。
+ * </p>
+ *<h2>示例：</h2>
+ * <pre>
+ *     public class MessageProcessorImpl implements MessageProcessor,NetMonitor{
  *
+ *     }
+ * </pre>
  * @author 三刀
  * @version V1.0.0
  */
@@ -20,15 +29,15 @@ public interface NetMonitor<T> {
 
 
     /**
-     * 数据读取过滤,可用于统计流量
+     * 监控触发本次读回调Session的已读数据字节数
      *
      * @param session  当前执行read的AioSession对象
-     * @param readSize 本次解码读取的数据长度
+     * @param readSize 已读数据长度
      */
     void readMonitor(AioSession<T> session, int readSize);
 
     /**
-     * 数据输出过滤,可用于统计流量
+     * 监控触发本次写回调session的已写数据字节数
      *
      * @param session   本次执行write回调的AIOSession对象
      * @param writeSize 本次输出的数据长度
