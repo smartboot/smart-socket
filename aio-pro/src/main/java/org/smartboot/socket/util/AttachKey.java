dc399722e8927eb6daefe5dac25178b4216bb5dc
三刀 <junwei.zheng@hipac.cn>
2019-11-12 22:33:22 +0800

    优化附件模型

--- aio-pro/src/main/java/org/smartboot/socket/util/AttachKey.java
+++ aio-pro/src/main/java/org/smartboot/socket/util/AttachKey.java
@@ -2,18 +2,25 @@ package org.smartboot.socket.util;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author 三刀
  * @version V1.0 , 2018/6/2
  */
-public class AttachKey<T> {
+public final class AttachKey<T> {
+    public static final int MAX_ATTACHE_COUNT = 128;
     private static final ConcurrentMap<String, AttachKey> NAMES = new ConcurrentHashMap<>();
-    private String key;
-    private T value;
+    private static final AtomicInteger index = new AtomicInteger(0);
+    private final String key;
+    private final int keyIndex;
 
     private AttachKey(String key) {
         this.key = key;
+        this.keyIndex = index.getAndIncrement();
+        if (this.keyIndex < 0 || this.keyIndex >= MAX_ATTACHE_COUNT) {
+            throw new RuntimeException("too many attach key");
+        }
     }
 
     public static <T> AttachKey<T> valueOf(String name) {
@@ -32,11 +39,8 @@ public class AttachKey<T> {
         return key;
     }
 
-    T getValue() {
-        return value;
-    }
 
-    void setValue(T value) {
-        this.value = value;
+    int getKeyIndex() {
+        return keyIndex;
     }
 }
--- aio-pro/src/main/java/org/smartboot/socket/util/Attachment.java
+++ aio-pro/src/main/java/org/smartboot/socket/util/Attachment.java
@@ -1,26 +1,33 @@
 package org.smartboot.socket.util;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * @author 三刀
  * @version V1.0 , 2018/6/1
  */
 public class Attachment {
-    private Map<String, AttachKey> attachKeyMap = new ConcurrentHashMap<>();
+
+    private Object[] values = new Object[8];
 
     public <T> void put(AttachKey<T> key, T t) {
-        key.setValue(t);
-        attachKeyMap.put(key.getKey(), key);
+        int index = key.getKeyIndex();
+        if (index > values.length) {
+            Object[] old = values;
+            int i = 1;
+            do {
+                i <<= 1;
+            } while (i < index);
+            values = new Object[i];
+            System.arraycopy(old, 0, values, 0, old.length);
+        }
+        values[index] = t;
     }
 
 
     public <T> T get(AttachKey<T> key) {
-        return (T) attachKeyMap.get(key.getKey()).getValue();
+        return (T) values[key.getKeyIndex()];
     }
 
     public <T> void remove(AttachKey<T> key) {
-        attachKeyMap.remove(key.getKey());
+        values[key.getKeyIndex()] = null;
     }
 }
