9859abeff6f7338a07d5e5e9171e379b3ec3dceb
三刀 <junwei.zheng@hipac.cn>
2019-11-16 16:38:07 +0800

    性能优化

--- aio-core/src/main/java/org/smartboot/socket/buffer/FastBufferThread.java
+++ aio-core/src/main/java/org/smartboot/socket/buffer/FastBufferThread.java
@@ -4,7 +4,7 @@ package org.smartboot.socket.buffer;
  * @author 三刀
  * @version V1.0 , 2019/11/16
  */
-class FastBufferThread extends Thread {
+final class FastBufferThread extends Thread {
     private final int index;
 
     public FastBufferThread(Runnable target, String name, int index) {
