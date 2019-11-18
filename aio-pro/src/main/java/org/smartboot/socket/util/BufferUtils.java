09cf8abebcab552baf60a78c08f9671aaa96aac2
三刀 <junwei.zheng@hipac.cn>
2019-11-18 21:18:06 +0800

    处理checkstyle问题

--- aio-pro/src/main/java/org/smartboot/socket/util/BufferUtils.java
+++ aio-pro/src/main/java/org/smartboot/socket/util/BufferUtils.java
@@ -7,10 +7,22 @@ import java.nio.ByteBuffer;
  * @version V1.0 , 2018/3/23
  */
 public class BufferUtils {
+    /**
+     * Horizontal space
+     */
     private static final byte SP = 32;
+    /**
+     * Carriage return
+     */
     private static final byte CR = 13;
+    /**
+     * Line feed character
+     */
     private static final byte LF = 10;
 
+    /**
+     * @param buffer
+     */
     public static void trim(ByteBuffer buffer) {
         int pos = buffer.position();
         int limit = buffer.limit();
@@ -34,6 +46,10 @@ public class BufferUtils {
         buffer.limit(limit);
     }
 
+    /**
+     * @param buffer
+     * @return
+     */
     public static short readUnsignedByte(ByteBuffer buffer) {
         return (short) (buffer.get() & 0xFF);
     }
