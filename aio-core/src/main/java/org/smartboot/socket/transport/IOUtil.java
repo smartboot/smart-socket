8923606e2674fb6b3ae7c3437985f342da80ea9e
三刀 <junwei.zheng@hipac.cn>
2019-12-02 20:20:55 +0800

    补充注释

--- aio-core/src/main/java/org/smartboot/socket/transport/IOUtil.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IOUtil.java
@@ -11,8 +11,14 @@ import java.nio.channels.AsynchronousSocketChannel;
  * @version V1.0 , 2019/12/2
  */
 final class IOUtil {
+    /**
+     * logger
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(IOUtil.class);
 
+    /**
+     * @param channel 需要被关闭的通道
+     */
     public static void close(AsynchronousSocketChannel channel) {
         if (channel == null) {
             throw new NullPointerException();
