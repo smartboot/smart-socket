81f2687fdbd3396769f62f76280c40e960f35af3
三刀 <junwei.zheng@hipac.cn>
2019-12-31 18:25:59 +0800

    更新版本号

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -38,7 +38,7 @@ final class IoServerConfig<T> {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.4.7.20191226-SNAPSHOT";
+    public static final String VERSION = "v1.4.7.20191231-SNAPSHOT";
 
     /**
      * 消息体缓存大小,字节
