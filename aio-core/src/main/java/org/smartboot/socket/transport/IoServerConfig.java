594e06746a2158b42e205c5ac60f133dcfd6eb9d
三刀 <zhengjunweimail@163.com>
2021-02-06 23:11:44 +0800

    发布正式包 1.5.5

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -39,7 +39,7 @@ final class IoServerConfig<T> {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.5.5-SNAPSHOT";
+    public static final String VERSION = "v1.5.5";
 
     /**
      * 消息体缓存大小,字节
