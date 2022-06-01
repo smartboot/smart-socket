068d38c1c0dac4ebe512a69b08c2c3fc96f3d7e2
三刀 <zhengjunweimail@163.com>
2022-06-01 21:54:33 +0800

    还原代码

--- aio-pro/src/main/java/org/smartboot/socket/transport/UdpChannel.java
+++ aio-pro/src/main/java/org/smartboot/socket/transport/UdpChannel.java
@@ -24,7 +24,6 @@ import java.nio.channels.Selector;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Semaphore;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 /**
@@ -150,7 +149,7 @@ public final class UdpChannel {
                     e.printStackTrace();
                 }
             };
-            WriteBuffer writeBuffer = new WriteBuffer(bufferPage, consumer, (virtualBuffers, writeBufferConsumer) -> false, config.getWriteBufferSize(), 1);
+            WriteBuffer writeBuffer = new WriteBuffer(bufferPage, consumer, config.getWriteBufferSize(), 1);
             return new UdpAioSession(UdpChannel.this, remote, writeBuffer);
         });
     }
