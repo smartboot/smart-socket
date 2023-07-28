20aa953c5781f146da2e04078ecde48f86387740
三刀 <zhengjunweimail@163.com>
2023-07-28 23:01:43 +0800

    v1.5.32

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -39,7 +39,7 @@ final class IoServerConfig {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.5.31";
+    public static final String VERSION = "v1.5.32";
 
     /**
      * 消息体缓存大小,字节
