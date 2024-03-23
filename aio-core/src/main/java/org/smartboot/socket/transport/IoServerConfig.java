123ff3196740ad35699dc775bc2ee91360ccac88
zhengjw22 <zhengjunweimail@163.com>
2024-03-23 20:25:05 +0800

    v1.5.43

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -39,7 +39,7 @@ final class IoServerConfig {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.5.43-SNAPSHOT";
+    public static final String VERSION = "v1.5.43";
 
     /**
      * 消息体缓存大小,字节
