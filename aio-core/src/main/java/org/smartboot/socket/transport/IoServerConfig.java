b60fa21ff37bb79689f441034f5142057f47d28d
zhengjw22 <zhengjunweimail@163.com>
2025-03-30 16:25:58 +0800

    1.5.58-SNAPSHOT

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -38,7 +38,7 @@ final class IoServerConfig {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.5.57";
+    public static final String VERSION = "v1.5.58";
 
     /**
      * 消息体缓存大小,字节
