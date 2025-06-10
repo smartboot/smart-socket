314cd2407ce75935429571e5b12bb7d1469970d8
zhengjw22 <zhengjunweimail@163.com>
2025-06-10 09:24:30 +0800

    build(version): 更新版本号至1.5.66-SNAPSHOT
    - 在 IoServerConfig.java 中更新 VERSION 常量- 在所有模块的 pom.xml 文件中更新版本号
    - 更新 Makefile 中的版本号

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -38,7 +38,7 @@ final class IoServerConfig {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.5.65";
+    public static final String VERSION = "v1.5.66-SNAPSHOT";
 
     /**
      * 消息体缓存大小,字节
