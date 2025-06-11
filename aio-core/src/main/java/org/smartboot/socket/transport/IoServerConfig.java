c463d94295d4d91e5b3ae1ba04de46074e23bb62
zhengjw22 <zhengjunweimail@163.com>
2025-06-11 11:25:39 +0800

    build: 更新版本号至 1.5.67
    - 修改了 aio-core、aio-pro、benchmark、example、smart-socket-parent 和根项目的 pom.xml 文件中的版本号
    - 更新了 IoServerConfig 类中的 VERSION 常量- 调整了 Makefile 中的版本号变量

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -38,7 +38,7 @@ final class IoServerConfig {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.5.67-SNAPSHOT";
+    public static final String VERSION = "v1.5.67";
 
     /**
      * 消息体缓存大小,字节
