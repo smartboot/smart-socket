b789edd071e4126255b310ce3ebffcb30cce820d
zhengjw22 <zhengjunweimail@163.com>
2025-12-25 16:50:59 +0800

    chore(version): 更新版本号为 1.8.1
    - 移除 aio-core 模块中的 SNAPSHOT 标记
    - 更新 smart-socket-parent 父项目版本号
    - 同步更新 aio-pro、benchmark、example 等子模块依赖版本
    - 更新 Makefile 中的版本变量定义

--- aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
+++ aio-core/src/main/java/org/smartboot/socket/transport/IoServerConfig.java
@@ -38,7 +38,7 @@ final class IoServerConfig {
     /**
      * 当前smart-socket版本号
      */
-    public static final String VERSION = "v1.8.1-SNAPSHOT";
+    public static final String VERSION = "v1.8.1";
 
     /**
      * 消息体缓存大小,字节
